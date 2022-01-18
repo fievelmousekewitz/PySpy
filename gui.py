cc68d33efe67b6d772d6b44e75b40840b4bf3259
Jeff Johnson <jeff.johnson@acti.aero>
2022-01-18 11:12:33 -0800

    Fixed compat issue with wxpy 4.1, added shortcut to hide menu

--- gui.py
+++ gui.py
@@ -288,6 +288,7 @@ class Frame(wx.Frame):
             self.SetMenuBar(None)
             self.hiddenmenu = True
 
+
         # Reset StayOnTop
         if self.options.Get("StayOnTop", True):
             self.ToggleWindowStyle(wx.STAY_ON_TOP)
